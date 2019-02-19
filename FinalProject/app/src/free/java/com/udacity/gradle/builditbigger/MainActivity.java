0b31fa56a59d39a5da6c787692c002055e3287ed
diegocotta <diego.cotta@zup.com.br>
2019-02-19 11:56:43 -0300

    add Interstitial Ads

--- FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -10,6 +10,10 @@ import android.view.Menu;
 import android.view.View;
 
 import com.example.showjoke.ShowJokeActivity;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.udacity.gradle.builditbigger.databinding.ActivityMainBinding;
 
 
@@ -19,12 +23,31 @@ import java.util.concurrent.ExecutionException;
 public class MainActivity extends AppCompatActivity implements EndpointsAsyncTask.AsyncResponse {
 
     ActivityMainBinding binding;
+    private InterstitialAd mInterstitialAd;
+    public String joke = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.ad_unit_Id));
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                callShowJoke();
+            }
+        });
 
+    }
+
+    private void callShowJoke() {
+        binding.progressBar.setVisibility(View.GONE);
+        Intent intent = new Intent(MainActivity.this, ShowJokeActivity.class);
+        intent.putExtra(ShowJokeActivity.JOKE_TEXT_KEY, joke);
+        startActivity(intent);
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
     }
 
     public void tellJoke(View view) throws ExecutionException, InterruptedException {
@@ -37,9 +60,12 @@ public class MainActivity extends AppCompatActivity implements EndpointsAsyncTas
 
     @Override
     public void processFinish(String Joke) {
-        Intent intent = new Intent(this, ShowJokeActivity.class);
-        intent.putExtra(ShowJokeActivity.JOKE_TEXT_KEY, Joke);
-        binding.progressBar.setVisibility(View.GONE);
-        startActivity(intent);
+        this.joke = Joke;
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            callShowJoke();
+        }
+
     }
 }
